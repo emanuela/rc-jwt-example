7547be0fd91a96d08436ff825de9b8a226577505
emanuelalongi <emanuel.alongi@gmail.com>
2020-07-23 15:07:21 -0600

    Initial commit of spring boot JWT example to new repository

---
+++ src/main/java/com/emanuel/springbootjwt/model/JwtResponse.java
@@ -0,0 +1,18 @@
+package com.emanuel.springbootjwt.model;
+
+import java.io.Serializable;
+
+public class JwtResponse implements Serializable {
+
+    private static final long serialVersionUID = -1;
+
+    private final String jwttoken;
+
+    public JwtResponse(String jwttoken) {
+        this.jwttoken = jwttoken;
+    }
+
+    public String getToken() {
+        return this.jwttoken;
+    }
+}
